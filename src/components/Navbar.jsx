dd0bd6519993ff59b2ba0b1df5afdaa86fab931f
abdiel-contreras <81264735+abdiel-contreras@users.noreply.github.com>
2026-02-10 18:25:04 -0600

    Create Navbar.jsx

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,8 @@
+export default function Navbar() {
+  return (
+    <nav style={{ background: '#2c3e50', color: 'white', padding: '1rem', display: 'flex', justifyContent: 'space-between' }}>
+      <strong>Framework Structure App</strong>
+      <span>Equipo de 3</span>
+    </nav>
+  );
+}
